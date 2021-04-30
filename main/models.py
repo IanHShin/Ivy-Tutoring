e62eb65f844ce5b73e376e39b82a49d5a9b767ae
pizza0201 <46835522+pizza0201@users.noreply.github.com>
2021-04-30 09:22:01 -0700

    Paypayl chekout, password reset/change

--- main/models.py
+++ main/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from taggit.managers import TaggableManager
 
-
 class User(AbstractUser):
 	email_confirm = models.BooleanField(default=False)
 
@@ -14,7 +13,6 @@ class Profile(models.Model):
 	city = models.CharField(max_length= 100,blank = True)
 	state = models.CharField(max_length = 100,blank = True)
 	tags = TaggableManager(blank = True)
-	
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
@@ -22,4 +20,13 @@ class Profile(models.Model):
 class OTP(models.Model):
 	email = models.EmailField(max_length=100, null=True, default=None)
 	token = models.CharField(max_length = 30, null=True, default=None)
+
+class Invoice(models.Model):
+	email = models.EmailField(max_length=100)
+	invoice_id = models.CharField(max_length=100, default=None)
+	pay_id = models.CharField(max_length=100)
+	amount = models.DecimalField(max_digits=6, decimal_places=2)
+	detail = models.CharField(max_length=100)
+	paid = models.BooleanField(default=False)
+
 
