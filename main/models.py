b934e982d4ab721635a6c5bc609e82f77136b6fe
iOSGenesis <Genesis.n.salinas@gmail.com>
2021-04-30 22:07:54 -0400

    Fixed fters in contact page , + footers to pages that didnt have it
    fixed footer in contact page since the icons were squared, added footers to pages that didn't have any except tutors.

--- main/models.py
+++ main/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from taggit.managers import TaggableManager
-from multiselectfield import MultiSelectField
 
 
 class User(AbstractUser):
